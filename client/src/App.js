1fb9425668e4f564ef4b6b8e2c8b38b4c4d417d3
Kirill20poshliy <qazasd2014@yandex.ru>
2024-01-03 15:00:06 +0300

    client-server finally

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,46 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import { useState } from "react"
 
-export default App;
+export default function App() {
+
+  const [usersList, setUsersList] = useState([])
+  const [usersCount, setUsersCount] = useState(0)
+
+  const usersHandler = async () => {
+    await fetch('http://localhost:8080/api/users')
+    .then(res => res.json())
+    .then(data => {
+      if (data.count) {
+        setUsersList(data.data)
+        setUsersCount(data.count)
+      }
+    })
+    .catch(e => {
+      setUsersList([e.message])
+    })
+  }
+
+  return (
+
+    <div className='App'>
+      <header>
+        <h2>Get users</h2>
+        <p>v1.0</p>
+      </header>
+      <section>
+        <button id='btn-primary' onClick={usersHandler}>
+          Получить список
+        </button>
+        <h2>Количество: {usersCount}</h2>
+        <ul>
+          {
+            usersList.length
+            ? usersList.map((user, idx) => <li key={idx}>{user}</li>)
+            : 'Список пользователей пуст!'
+          }
+        </ul>
+      </section>
+    </div>
+
+  )
+
+}
